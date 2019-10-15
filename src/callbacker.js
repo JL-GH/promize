8a7e5bf7fa9e5c214d60f54167772aa6c28c3286
JL-GH <jlingh1993@gmail.com>
2019-10-15 00:29:48 -0400

    upload

--- src/callbacker.js
+++ src/callbacker.js
@@ -1,5 +1,23 @@
-const syncCallbacker = null;
+const syncCallbacker = (arg1, arg2) => {
+  if (typeof arg1 !== 'function' || typeof arg2 !== 'function') {
+    throw ''
+  }
+  let result1 = arg1()
+  return arg2(result1)
+};
 
-const asyncCallbacker = null;
+const asyncCallbacker = (arg1, arg2) => {
+  if (typeof arg1 !== 'function' || typeof arg2 !== 'function') {
+    throw ''
+  }
+  let data = ''
+
+  let done = (data) => {
+    arg2(data, done)
+  }
+
+  arg1(data, done)
+}
 
 module.exports = { syncCallbacker, asyncCallbacker };
+
--- src/promise.js
+++ src/promise.js
@@ -1,6 +1,15 @@
-const basicPromise = null;
-const funcPromise = () => null;
-const chainedPromise = prom => null;
+const basicPromise = new Promise((resolve, reject) => {
+  resolve('basicPromise')
+})
+const funcPromise = () => {
+  let prom = new Promise((resolve, reject) => {
+    resolve('funcPromise')
+  })
+  return prom
+};
+const chainedPromise = prom => {
+
+};
 const rejectedPromise = () => null;
 
 module.exports = { basicPromise, chainedPromise, rejectedPromise, funcPromise };
